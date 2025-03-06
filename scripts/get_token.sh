36fb2cb913f4738adbb41040e6989be38c6dbeb5
Duda <maria.oliveira@zup.com.br>
2025-03-06 10:50:27 -0300

    editing shell script, to run in linux

--- scripts/get_token.sh
+++ scripts/get_token.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 TOKEN_FILE="./scripts/token.txt"
-JQ_PATH="/c/Users/Zupper/jq/jq.exe"  # Caminho do jq
 API_URL='https://idm.stackspot.com/zup/oidc/oauth/token'
 CLIENT_ID='387c39a2-3876-43fa-9b1b-848a44213100'
 CLIENT_SECRET='9MbJ68x4R22Xp1p7mwOPMpi1bDiYM42S78Dp7BgJQCJljkxU04x7h7YMo9ewmoO3'
+
+# Verifica se o jq está instalado
+if ! command -v jq &> /dev/null; then
+    echo "❌ Erro: 'jq' não está instalado. Instale-o com 'sudo apt install jq' ou 'sudo yum install jq'."
+    exit 1
+fi
 
 # Função para obter um novo token
 gerar_novo_token() {
@@ -16,12 +21,12 @@ gerar_novo_token() {
         --data-urlencode "client_secret=$CLIENT_SECRET")
 
     # Verifica se a resposta é um JSON válido
-    if echo "$RESPONSE" | "$JQ_PATH" . > /dev/null 2>&1; then
-        NEW_TOKEN=$(echo "$RESPONSE" | "$JQ_PATH" -r '.access_token')
-        
+    if echo "$RESPONSE" | jq . > /dev/null 2>&1; then
+        NEW_TOKEN=$(echo "$RESPONSE" | jq -r '.access_token')
+
         if [ -n "$NEW_TOKEN" ] && [ "$NEW_TOKEN" != "null" ]; then
             echo "$NEW_TOKEN" > "$TOKEN_FILE"
-            echo "✅ Novo token salvo."
+            echo "✅ Novo token salvo em $TOKEN_FILE."
         else
             echo "❌ Erro ao obter o novo token! A resposta da API não contém um token válido."
             exit 1
